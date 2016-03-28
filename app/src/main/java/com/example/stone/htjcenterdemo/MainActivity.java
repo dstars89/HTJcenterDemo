6dcaa559cf5823f4fff8c51f6972e91096300bf4
hzfuguangxin <hzfuguangxin@corp.netease.com>
2016-03-28 17:53:46 +0800

    UiMode的方式实现夜间模式demo

--- app/src/main/java/com/example/stone/htjcenterdemo/MainActivity.java
+++ app/src/main/java/com/example/stone/htjcenterdemo/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.stone.htjcenterdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("ConstantConditions")
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private NightModeHelper mNightModeHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mNightModeHelper = new NightModeHelper(this);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                mNightModeHelper.toggle();//可以在recreate()的时候,截图做动画提高用户体验
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }
